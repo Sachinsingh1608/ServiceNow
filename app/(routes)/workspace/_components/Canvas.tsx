432977b62e5cff395bb537fca9cd947922040565
Sachinsingh1608 <sk379403@gmail.com>
2024-03-24 02:09:24 +0530

    main

--- app/(routes)/workspace/_components/Canvas.tsx
+++ app/(routes)/workspace/_components/Canvas.tsx
@@ -1,68 +1,39 @@
-"use client"
-import React, { useEffect, useState } from 'react'
-import {  LiveCollaborationTrigger, MainMenu, WelcomeScreen } from "@excalidraw/excalidraw";
-import dynamic from "next/dynamic";
+"use client";
+import { Tldraw } from 'tldraw'
+import './index.css'
 
-const Excalidraw = dynamic(
-  async () => (await import("@excalidraw/excalidraw")).Excalidraw,
-  {
-    ssr: false,
-  },
-);
 
-// Custom Logo component
+
 const CustomLogo = () => {
   return (
     <img src="/ServiceNow_logo.svg.png" 
-
-    height={100}
-    width={100}
-    alt="Custom Logo" />
+      height={100}
+      width={100}
+      alt="Custom Logo" />
   );
 };
+
 const MainLogo = () => {
-    return (
-      <img src="/ServiceNow_logo.svg.png" 
-  
+  return (
+    <img src="/ServiceNow_logo.svg.png" 
       height={500}
       width={500}
       alt="Custom Logo" />
-    );
-  };
+  );
+};
 
 function Canvas() {
+
+
   return (
-    <div style={{ height: "800px" }}>
-      <Excalidraw
-        onChange={(excalidrawElements, appState, files) => (excalidrawElements)}
-        UIOptions={{
-          canvasActions: {
-            saveToActiveFile: false,
-            loadScene: true,
-          }
-        }}
-      >
-        <MainMenu>
-        <CustomLogo />
-          <MainMenu.DefaultItems.ClearCanvas/>
-        
-          <MainMenu.DefaultItems.Export/>
-          <MainMenu.DefaultItems.SaveAsImage/>
-          <MainMenu.DefaultItems.ToggleTheme/>
-          <MainMenu.DefaultItems.ChangeCanvasBackground/>
-        </MainMenu>
-        <WelcomeScreen>
-          <WelcomeScreen.Hints.MenuHint/>
-          <WelcomeScreen.Hints.MenuHint/>
-          <WelcomeScreen.Hints.ToolbarHint/>
-          <WelcomeScreen.Center>
-            
-           <MainLogo/>
-          </WelcomeScreen.Center>
-        </WelcomeScreen>
-      </Excalidraw>
-    </div>
-  )
+    <div>
+
+
+    <div style={{ position: 'fixed', inset: 0 }}>
+    <Tldraw />
+  </div>
+  </div>
+  );
 }
 
 export default Canvas;
--- app/(routes)/workspace/_components/WorkspaceHeader.tsx
+++ app/(routes)/workspace/_components/WorkspaceHeader.tsx
@@ -1,24 +1,12 @@
-import { Button } from '@/components/ui/button'
-//@ts-ignore
-import { Link, Save } from 'lucide-react'
-import Image from 'next/image'
+
+
 import React from 'react'
 
-function WorkspaceHeader({onSave}:any) {
+function WorkspaceHeader() {
   return (
-    <div className='p-3 border-b flex justify-between items-center'>
-      <div className='flex gap-2 items-center'>
-        <Image src={'/ServiceNow_logo.svg.png'}
-          alt='logo'
-          height={200}
-          width={200} />
-        
-      </div>
-      <div className='flex items-center gap-4'>
-        
-       
-      </div>
-    </div>
+    <>
+    </>
+    
   )
 }
 
