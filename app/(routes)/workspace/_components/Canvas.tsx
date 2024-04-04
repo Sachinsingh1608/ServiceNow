bc9b5592d39c7e1964c9ff23c6890519a7bfbcb2
Sachinsingh1608 <109678338+Sachinsingh1608@users.noreply.github.com>
2024-04-04 07:34:35 +0530

    Update Canvas.tsx

--- app/(routes)/workspace/_components/Canvas.tsx
+++ app/(routes)/workspace/_components/Canvas.tsx
@@ -1,39 +1,68 @@
-"use client";
-import { Tldraw } from 'tldraw'
-import './index.css'
+"use client"
+import React, { useEffect, useState } from 'react'
+import {  LiveCollaborationTrigger, MainMenu, WelcomeScreen } from "@excalidraw/excalidraw";
+import dynamic from "next/dynamic";
 
+const Excalidraw = dynamic(
+  async () => (await import("@excalidraw/excalidraw")).Excalidraw,
+  {
+    ssr: false,
+  },
+);
 
-
+// Custom Logo component
 const CustomLogo = () => {
   return (
     <img src="/ServiceNow_logo.svg.png" 
-      height={100}
-      width={100}
-      alt="Custom Logo" />
+
+    height={100}
+    width={100}
+    alt="Custom Logo" />
   );
 };
-
 const MainLogo = () => {
-  return (
-    <img src="/ServiceNow_logo.svg.png" 
+    return (
+      <img src="/ServiceNow_logo.svg.png" 
+  
       height={500}
       width={500}
       alt="Custom Logo" />
-  );
-};
+    );
+  };
 
 function Canvas() {
-
-
   return (
-    <div>
-
-
-    <div style={{ position: 'fixed', inset: 0 }}>
-    <Tldraw />
-  </div>
-  </div>
-  );
+    <div style={{ height: "800px" }}>
+      <Excalidraw
+        onChange={(excalidrawElements, appState, files) => (excalidrawElements)}
+        UIOptions={{
+          canvasActions: {
+            saveToActiveFile: false,
+            loadScene: true,
+          }
+        }}
+      >
+        <MainMenu>
+        <CustomLogo />
+          <MainMenu.DefaultItems.ClearCanvas/>
+        
+          <MainMenu.DefaultItems.Export/>
+          <MainMenu.DefaultItems.SaveAsImage/>
+          <MainMenu.DefaultItems.ToggleTheme/>
+          <MainMenu.DefaultItems.ChangeCanvasBackground/>
+        </MainMenu>
+        <WelcomeScreen>
+          <WelcomeScreen.Hints.MenuHint/>
+          <WelcomeScreen.Hints.MenuHint/>
+          <WelcomeScreen.Hints.ToolbarHint/>
+          <WelcomeScreen.Center>
+            
+           <MainLogo/>
+          </WelcomeScreen.Center>
+        </WelcomeScreen>
+      </Excalidraw>
+    </div>
+  )
 }
 
 export default Canvas;
